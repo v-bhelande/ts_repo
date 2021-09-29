818caf276bf2fe31250cf2b74011e526941cfa51
bryancfoo <68606269+bryancfoo@users.noreply.github.com>
2021-09-28 21:47:09 -0400

    Update thomson.py

--- plasmapy/diagnostics/thomson.py
+++ plasmapy/diagnostics/thomson.py
@@ -1337,10 +1337,6 @@ def _scattered_power_model_arbdist(wavelengths, settings=None, **params):
 
     # Create VDFs from model functions
 
-    # The 0 indices here are a really bad fix; definitely get a better solution later
-    ve = settings["e_velocity_axes"][0]
-    vi = settings["i_velocity_axes"][0]
-
     fe = emodel(ve, **eparams)
     fi = imodel(vi, **iparams)
 

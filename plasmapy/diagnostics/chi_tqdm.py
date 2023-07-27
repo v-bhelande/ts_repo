999a7cf2e4d913fc9108c8824f4b6651ca6ec3d8
v-kothale <107497019+v-kothale@users.noreply.github.com>
2023-07-27 11:55:53 -0700

    Update chi_tqdm.py

--- plasmapy/diagnostics/chi_tqdm.py
+++ plasmapy/diagnostics/chi_tqdm.py
@@ -24,6 +24,7 @@ import warnings
 from lmfit import Model
 # from numba import jit
 from typing import List, Tuple, Union
+from tqdm import tqdm
 
 from plasmapy.formulary.dielectric import fast_permittivity_1D_Maxwellian
 from plasmapy.formulary.parameters import fast_plasma_frequency, fast_thermal_speed

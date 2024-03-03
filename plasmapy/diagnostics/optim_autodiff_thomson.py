a67f89506f95396665f8349ec72a373e20ca3c70
v-bhelande <107497019+v-bhelande@users.noreply.github.com>
2024-03-02 19:59:07 -0800

    Update optim_autodiff_thomson.py
    Commented out None on lines 62 and 63, let's see if that gets things to work.

--- plasmapy/diagnostics/optim_autodiff_thomson.py
+++ plasmapy/diagnostics/optim_autodiff_thomson.py
@@ -59,8 +59,8 @@ def torch_1d_interp(
     x: torch.Tensor,
     xp: torch.Tensor,
     fp: torch.Tensor,
-    left: float | None = None,
-    right: float | None = None,
+    left: float #| None = None,
+    right: float #| None = None,
 ) -> torch.Tensor:
 
     """
